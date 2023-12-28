e5465c9e9bfc128a155f9eee78ffbed3b2f7e4dc
dhia-zeddini <dhia.zeddini.14@gmail.com>
2023-12-28 20:14:41 +0100

    player details

--- StoryBoard/DetailsViewController.swift
+++ StoryBoard/DetailsViewController.swift
@@ -13,14 +13,43 @@ class DetailsViewController: UIViewController {
     //var
     var playerName:String?
     var playerColor:String?
+    
+    //widgets
 
+    @IBOutlet weak var playerAvatar: UIImageView!
+    
+    //life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        title=playerName!
 
-        // Do any additional setup after loading the view.
+        playerAvatar.image=UIImage(named: playerColor!)
     }
     
 
+    //methods
+    func isSuspect() -> Bool {
+        return true
+    }
     
-
+    func showAlert(title:String,message:String) {
+        let alert=UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(action)
+        self.present(alert, animated: true)
+    }
+    
+    //IBActions
+    
+    @IBAction func suspect(_ sender: Any) {
+        
+        if(isSuspect()){
+            
+            
+            
+        }else{
+            self.showAlert(title: "Warning", message: "Player already marked as suspect")
+        }
+    }
+    
 }
