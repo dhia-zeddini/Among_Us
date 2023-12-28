418b45b7ce6497e06f97407f4f12e894f58dc01e
dhia-zeddini <dhia.zeddini.14@gmail.com>
2023-12-28 19:55:31 +0100

    players list

--- StoryBoard/DetailsViewController.swift
+++ StoryBoard/DetailsViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    
+    //var
+    var playerName:String?
+    var playerColor:String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,14 +21,6 @@ class DetailsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- StoryBoard/HomeViewController.swift
+++ StoryBoard/HomeViewController.swift
@@ -7,26 +7,74 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController ,UITableViewDelegate, UITableViewDataSource{
+   
+    
     
     //var
     var avatarName:String?
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var players=["Hu5tl3r", "MC_Killer", "CR7", "Not_Impostor", "BabyShark", "AKINFENWA", "DR'Who"]
+    var playerrsColors=["Red","Orange","Brown","Rose","Yellow","Light-blue","Purple"]
+    
+    //widgets
+    
+    @IBOutlet weak var helloLabel: UILabel!
+    
+    @IBOutlet weak var playersTv: UITableView!
+    
+    //TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        return players.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=tableView.dequeueReusableCell(withIdentifier: "mCell")
+        let cv=cell?.contentView
+        
+        let playerAvatar=cv?.viewWithTag(1) as! UIImageView
+        let playerName=cv?.viewWithTag(2) as! UILabel
+        let playerColor=cv?.viewWithTag(3) as! UILabel
+        
+        playerAvatar.image=UIImage(named: playerrsColors[indexPath.row])
+        playerName.text=players[indexPath.row]
+        playerColor.text=playerrsColors[indexPath.row]
+        
+        return cell!
     }
-    */
+    
+    //cell select
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        performSegue(withIdentifier: "secondSegue", sender: indexPath)
+    }
+    
+    
+    //life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        helloLabel.text="Hello "+avatarName!
+    }
+    
+    //methods
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier=="secondSegue"{
+            let index=sender as! IndexPath
+            let destination=segue.destination as! DetailsViewController
+            destination.playerName=players[index.row]
+            destination.playerColor=playerrsColors[index.row]
+        }
+    }
 
+    
+    //IBActions
+    
+    
+    @IBAction func emergencyBtn(_ sender: Any) {
+    }
+    
 }
